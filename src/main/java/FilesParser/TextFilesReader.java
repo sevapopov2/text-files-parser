c83e64ed20e1d5bfb127e635f2d49c93c707a14c
Vsevolod Popov <sevapopov13@gmail.com>
2023-02-14 11:30:02 +0300

    Fix type mismatches. Remove unused imports.

--- src/main/java/FilesParser/TextFilesReader.java
+++ src/main/java/FilesParser/TextFilesReader.java
@@ -3,7 +3,6 @@ package FilesParser;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,15 +18,16 @@ public class TextFilesReader implements Reader {
     }
 
     public List<Packet> startParse() {
-        List<String> result = new ArrayList<>();
+        List<Packet> result = new ArrayList<>();
 
         try {
             BufferedReader fileReader = new BufferedReader(new FileReader(this.filePath));
             while (fileReader.ready()) {
                 String line = fileReader.readLine();
-                Packet packet = new Packet(line.split("; ")[0], line.split("; ")[1], line.split("; ")[2]);
-                result.add(line);
+                Packet packet = new Packet(line.split("; ")[0], line.split("; ")[1], Integer.parseInt(line.split("; ")[2]));
+                result.add(packet);
             }
+            fileReader.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
